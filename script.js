3b5d47f98dfda8a6675e77e877be8a2b2117d19d
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-11-13 19:47:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+let fizzbuzz = (start, end) => {
+  // Write your code here
+
+  let result = [];
+
+  for (let i = start; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      result.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      result.push("Fizz");
+    } else if (i % 5 === 0) {
+      result.push("Buzz");
+    } else {
+      result.push(i.toString());
+    }
+  }
+
+  return result.join("\n");
+};
+
+// Do not change the code below
+
+const start = prompt("Enter the starting number - ");
+const end = prompt("Enter the ending number - ");
+alert(fizzbuzz(parseInt(start), parseInt(end)));
